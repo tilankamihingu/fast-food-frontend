d466fc6de3c8414056389611648662cf3766733d
tilankamihingu <tilankamihingu2018@gmail.com>
2021-09-05 02:05:22 -0700

    home content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,15 @@ const Home = () => {
     return (
         <div className='full'>
             <div className="home-container">
-
+                <div className="home-content">
+                    <h1>Fast food Paradize</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident<br /> itaque sit nostrum impedit unde illum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, libero?</p>
+                    <div className="content-btn">
+                        <button className="view">View Menu</button>
+                        <button className="order">Order Now</button>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
