d8f160d5c650a77b2dcd999220b1219b0b53d617
tilankamihingu <tilankamihingu2018@gmail.com>
2021-09-05 23:28:48 -0700

    two sections  add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 
+
 const Home = () => {
     return (
         <div className='full'>
@@ -12,6 +13,27 @@ const Home = () => {
                         <button className="view">View Menu</button>
                         <button className="order">Order Now</button>
                     </div>
+                </div>
+            </div>
+            <div className="pizza-container">
+                <div className="pizza-content">
+                    <h1>Sri Lankan Special Pizza</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis<br /> quod quibusdam officiis corporis suscipit neque architecto totam dolorem laborum est!</p>
+                    <div className="pizza-btn">
+                        <button className="pizza-view">View Menu</button>
+                        <button className="pizza-order">Order Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="about-container">
+                <div className="about-content">
+                    <h1>About our PARADIZE</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam<br /> doloribus quae nulla magni assumenda optio quas dolorum in odit nobis.<br /> Delectus dignissimos alias id numquam sint, sapiente saepe cum neque.</p>
+                    <div className="about-btn">
+                        <button className="about-direct">Direction</button>
+                    </div>
+                </div>
+                <div className="about-icons">
                     
                 </div>
             </div>
