2e9b1001d533e94a413e8234f66419f279af08cc
tilankamihingu <tilankamihingu2018@gmail.com>
2021-09-06 07:48:57 -0700

    product page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './Products.css';
+import Burger from '../../images/burger4.jpg';
 
 const Products = () => {
     return (
-        <div>
-            <h1>Products</h1>
+        <div className="product-full">
+           <div className="product-container">
+               <div className="product-row">
+                   <div className="product-card">
+                        <img src={Burger} alt="" />
+                       <div className="card-details">
+                           <h2>Name</h2>
+                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                       </div>
+                   </div>
+                   <div className="product-card">
+                        <img src={Burger} alt="" />
+                       <div className="card-details">
+                           <h2>Name</h2>
+                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                       </div>
+                   </div>
+                   <div className="product-card">
+                        <img src={Burger} alt="" />
+                       <div className="card-details">
+                           <h2>Name</h2>
+                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                       </div>
+                   </div>
+               </div>
+           </div>
         </div>
     )
 }
